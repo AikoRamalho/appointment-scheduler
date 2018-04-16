bbc19b561ff983c89ca22761b02dae5ead31eb94
Aiko Ramalho <aiko269@hotmail.com>
2018-04-16 17:32:34 -0300

    Add mongoose connection

--- api/app.js
+++ api/app.js
@@ -3,11 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('aiko:123@ds247449.mlab.com:47449/appointments', {
+  useMongoClient: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
